6d0ec21f5c75f2f21c6e9ec3f939e0d552585fe0
Yuri Almeida <yurilima95@gmail.com>
2020-11-18 15:19:14 -0300

    First game

---
+++ The_project/Project/Assets/Samples/maze runner/Scripts/Moviment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moviment : MonoBehaviour
+{
+    
+    private Rigidbody rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    public float speed = 5;
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rig.velocity = new Vector3(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rig.velocity = new Vector3(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rig.velocity = new Vector3(-speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rig.velocity = new Vector3(speed, 0f, 0f);
+        }
+
+        
+
+    }
+}
