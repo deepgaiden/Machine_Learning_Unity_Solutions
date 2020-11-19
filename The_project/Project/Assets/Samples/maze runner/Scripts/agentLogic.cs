ec352e709f335f244fe40dba575a174f9ba35769
Yuri Almeida <yurilima95@gmail.com>
2020-11-19 17:36:02 -0300

    Inverse Kinetics aproach

---
+++ The_project/Project/Assets/Samples/maze runner/Scripts/agentLogic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+
+public class agentLogic : Agent
+{
+
+    Rigidbody rig;
+
+
+    public override void Initialize() 
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        base.OnActionReceived(vectorAction);
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        base.Heuristic(actionsOut);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        base.OnEpisodeBegin();
+    }
+
+
+}
